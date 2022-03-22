a2466b8676673bb78e8d4897e361f7561ff8cced
이보람 <boram@ibolam-ui-MacBookAir.local>
2022-03-22 19:45:49 +0900

    Chrome App

--- js/weather.js
+++ js/weather.js
@@ -7,7 +7,6 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then(response => response.json()).then(data => {
-        console.log(data);
         // console.log(data.name, data.weather[0].main); // weather은 array임
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
